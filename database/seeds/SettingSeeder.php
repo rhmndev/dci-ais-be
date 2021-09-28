d0e6109cb3704e6f1cce7efcb5253b2c2931580a
Muhammad Taufik Hidayat <muhammad.taufik@arkamaya.co.id>
2021-09-28 10:45:01 +0700

    Gudang Seeder

--- database/seeds/SettingSeeder.php
+++ database/seeds/SettingSeeder.php
@@ -50,6 +50,24 @@ class SettingSeeder extends Seeder
                 'created_by' => 'seeder',
                 'changed_by' => 'seeder'
             ],
+            [
+                'variable' => 'Gudang',
+                'value' => json_decode('[
+                    {"id":0.9774112388693914,"name":"CH01;CHEMICAL"}
+                    {"id":0.7169090400554472,"name":"CO01;CONSUMABLE"}
+                    {"id":0.6666342468858273,"name":"CS01;CUSTOMER SUPPLY"}
+                    {"id":0.6246999578574441,"name":"DI01;DISCONTINUE"}
+                    {"id":0.6584882123637634,"name":"EN01;ENGINEERING"}
+                    {"id":0.1769217311649797,"name":"GS01;GS / ATK"}
+                    {"id":0.1204737882898632,"name":"MT01;MAINTENANCE"}
+                    {"id":0.14935756956400503,"name":"OH01;OHP (OUT HOUSE P"}
+                    {"id":0.07490602284114334,"name":"PB01;POOLING BUYING M"}
+                    {"id":0.97406668456854,"name":"PB02;POOLING BUYING F"}
+                    {"id":0.5598219072063504,"name":"PE01;PROD. ENGINEERIN"}
+                ]'),
+                'created_by' => 'seeder',
+                'changed_by' => 'seeder'
+            ],
         ];
 
         foreach ($datas as $data) {
